35892a229fa78eb818fbe30b54110524ef33b628
Blanc Ludovic <ludovic.blanc2000@gmail.com>
2018-01-31 21:50:33 +0000

    adding exercise 8

---
+++ bla/08_blurring.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+def show(title, img):
+    cv2.imshow(title, img)
+    cv2.imwrite("output/"+'08_'+title+".jpg", img)
+
+
+path = "../images/trex.png"
+image = cv2.imread(path)
+
+blurred = np.hstack([
+    cv2.blur(image, (3, 3)),
+    cv2.blur(image, (5, 5)),
+    cv2.blur(image, (7, 7))])
+show("Averaged", blurred)
+
+blurred = np.hstack([
+    cv2.GaussianBlur(image, (3, 3), 0),
+    cv2.GaussianBlur(image, (5, 5), 0),
+    cv2.GaussianBlur(image, (7, 7), 0)])
+show("Gaussian", blurred)
+
+blurred = np.hstack([
+    cv2.medianBlur(image, 3),
+    cv2.medianBlur(image, 5),
+    cv2.medianBlur(image, 7)])
+show("Median", blurred)
+
+blurred = np.hstack([
+    cv2.bilateralFilter(image, 5, 21, 21),
+    cv2.bilateralFilter(image, 7, 31, 31),
+    cv2.bilateralFilter(image, 9, 41, 41)])
+show("Bilateral", blurred)
+cv2.waitKey()
